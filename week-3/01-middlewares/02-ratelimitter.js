f3cbdc8fa374977183cbd59202659122a260d34d
Sarthak Sonawane <sarthak20.sonawane@gmail.com>
2026-02-16 14:52:18 +0530

    users server request rate limitter

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,23 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function (req,res,next){
+  const userId = req.headers["user-id"];
+
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]++;
+    if (numberOfRequestsForUser[userId]> 5) {
+      res.status(404).send("No entry for user now")
+    } else {
+      next()
+    }
+
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
